9e825d5e6ad52e803329fbbdbc7662350fb42abe
Park Ian Co <opensource@parkian.co>
2026-03-15 00:19:24 -0400

    Relicense from BSD-3-Clause to Apache-2.0

--- src/class-groups.lisp
+++ src/class-groups.lisp
@@ -1,5 +1,5 @@
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; class-groups.lisp - Binary Quadratic Form Operations for Class Group VDF
 
--- test/test-vdf.lisp
+++ test/test-vdf.lisp
@@ -1,10 +1,10 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;; SPDX-License-Identifier: BSD-3-Clause
+;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; test-vdf.lisp - Unit tests for vdf
 ;;;;
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 (defpackage #:cl-vdf.test
   (:use #:cl)
